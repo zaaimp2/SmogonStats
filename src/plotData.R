9617f71bcda330686615cd32512b21c9ac1cfe96
Zaaim Patel <zaaimp2@illinois.edu>
2021-06-10 22:36:06 -0400

    Added plotting function for individual pokemon stats

--- src/plotData.R
+++ src/plotData.R
@@ -12,11 +12,30 @@ plotTopNPokemon = function(data, topN = 10, dateAnalyzed){
   if(length(dateAnalyzed) >= 2){
     return("Error: Select only one date")
   }
-  
   data %>% 
-    filter(date == datesAnalyzed) %>% 
+    filter(date == dateAnalyzed) %>% 
     filter(Rank <= topN) %>% 
-    ggplot(aes(x = Pokemon, y = UsagePercentage)) +
+    ggplot(aes(x = reorder(Pokemon, UsagePercentage), y = UsagePercentage, fill = Pokemon)) +
     geom_col() +
-    coord_flip()
+    coord_flip() +
+    xlab("Pokemon") +
+    ylab("Usage Percentage")
 }
+
+plotTopNIndividualStat = function(cleanedJsonOutput, topN = 10, desiredStatistic){
+  if(desiredStatistic == "Checks and Countesrs"){
+    outputVariable = "forceOutRate"
+  } else {
+    outputVariable = "Usage"
+  }
+  cleanedJsonOutput %>% 
+    arrange(desc(get(outputVariable))) %>% 
+    filter(row_number() <= topN) %>% 
+    ggplot(aes(x = reorder(get(desiredStatistic), get(outputVariable)), y = get(outputVariable), fill = get(desiredStatistic))) +
+    geom_col() +
+    coord_flip() +
+    xlab(desiredStatistic) +
+    ylab(outputVariable) +
+    labs(fill = desiredStatistic)
+}
+
